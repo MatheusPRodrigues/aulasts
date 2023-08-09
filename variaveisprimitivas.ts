e5a78044032f0702a9aebcde7d8f9cdd44953e48
MatheusPRodrigues <matherodrigues17@gmail.com>
2023-08-09 19:34:44 -0300

    Meu primeiro programa

---
+++ variaveisprimitivas.ts
@@ -0,0 +1,15 @@
+var aux: number = 100;
+var aux1: boolean = true;
+var aux2: string = "Yooh";
+var aux5: void;
+var aux6: null = null;
+var aux7: undefined = undefined;
+enum aux8 { Red, Blue, Green};
+
+console.log(aux);
+console.log(aux1);
+console.log(aux2);
+console.log(aux5);
+console.log(aux6);
+console.log(aux7);
+console.log(aux8);
